e3baea7cacca295bfe71382c5d6c07794fe637b0
domko95 <dominique.kolbe1995@googlemail.com>
2020-10-09 13:32:39 +0200

    Add welcome screen

---
+++ src/components/pagination/pagination.stories.js
@@ -0,0 +1,9 @@
+import "./pagination.css";
+import pagination from "./pagination.html";
+
+export default {
+  title: "Components/Pagination",
+  parameters: { layout: "centered" },
+};
+
+export const Pagination = () => pagination;
