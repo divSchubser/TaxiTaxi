40d75227cf31ffe40a5a14973049a3efdff65b45
bwnstk <info@flimmerkiste.xyz>
2020-10-09 15:38:54 +0200

    Add HomeIndicator

---
+++ src/components/home-indicator/home-indicator.stories.js
@@ -0,0 +1,9 @@
+import "./home-indicator.css";
+import indicatorHome from "./home-indicator.html";
+
+export default {
+  title: "Components/Indicator",
+  parameters: { layout: "centered" },
+};
+
+export const homeIndicator = () => indicatorHome;
